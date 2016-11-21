6529632db839e8073a0cbf95fc884075ae25f831
skeyboy <lylapp@163.com>
2016-11-21 10:01:57 +0800

    Initial Commit

---
+++ AppLibs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppLibs
+//
+//  Created by liyulong on 2016/11/21.
+//  Copyright © 2016年 gener-tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppLibs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppLibs
+//
+//  Created by liyulong on 2016/11/21.
+//  Copyright © 2016年 gener-tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
